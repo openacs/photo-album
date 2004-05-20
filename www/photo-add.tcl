0a8decd181fe265376112260c0664abe8db7c431
Lars Pind <lars@pinds.com>
2004-05-20 10:09:44 +0000

    Removed template::

--- www/photo-add.tcl
+++ www/photo-add.tcl
@@ -27,28 +27,28 @@ set context_list [pa_context_bar_list -final "[_ photo-album._Upload]" $album_id
 
 set photo_id [db_string get_next_object_id "select acs_object_id_seq.nextval from dual"]
 
-template::form create photo_upload -action photo-add-2 -html {enctype multipart/form-data}
+form create photo_upload -action photo-add-2 -html {enctype multipart/form-data}
 
-template::element create photo_upload album_id  \
+element create photo_upload album_id  \
   -label "album_id" -datatype integer -widget hidden
 
-template::element create photo_upload photo_id  \
+element create photo_upload photo_id  \
   -label "photo_id" -datatype integer -widget hidden
 
-template::element create photo_upload upload_file  \
+element create photo_upload upload_file  \
   -label "[_ photo-album._Choose_2]" -help_text "[_ photo-album._Use]" -datatype text -widget file 
 
-template::element create photo_upload caption -html { size 30 } \
+element create photo_upload caption -html { size 30 } \
     -label "[_ photo-album.Caption]" -optional -help_text "[_ photo-album.lt_OPTIONAL_Displayed_on]" -datatype text 
 
-template::element create photo_upload description -html { size 50} \
+element create photo_upload description -html { size 50} \
   -label "[_ photo-album._Photo]" -optional -help_text "[_ photo-album.lt_OPTIONAL_Displayed_wh]" -datatype text
 
-template::element create photo_upload story -html { cols 50 rows 4 wrap soft } \
+element create photo_upload story -html { cols 50 rows 4 wrap soft } \
   -label "[_ photo-album._Photo_1]" -optional -help_text "[_ photo-album.OPTIONAL]" -datatype text -widget textarea
 
-template::element set_properties photo_upload album_id -value $album_id
-template::element set_properties photo_upload photo_id -value $photo_id
+element set_properties photo_upload album_id -value $album_id
+element set_properties photo_upload photo_id -value $photo_id
 
 ad_return_template
 
