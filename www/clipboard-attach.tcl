fb0bce1a634caa5bcf9d67b5897bcf50a9c1f74b
Jeff Davis <davis@xarg.net>
2005-03-01 00:01:40 +0000

    change ad_verify_and_get_user_id to ad_conn user_id, change ad_maybe_redirect_for_registration to auth::require_login, make some db_ and dt_ function -public

--- www/clipboard-attach.tcl
+++ www/clipboard-attach.tcl
@@ -12,7 +12,7 @@ ad_page_contract {
     collection_id:integer,notnull
 }
 
-ad_maybe_redirect_for_registration
+auth::require_login
 
 set user_id [ad_conn user_id]
 set peeraddr [ad_conn peeraddr]
